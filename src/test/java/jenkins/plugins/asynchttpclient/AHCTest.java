9150d9a92ba0c2a5c8e91dc624afc1f5fb5dca76
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-12-04 09:44:01 +0000

    prepare for release

--- src/test/java/jenkins/plugins/asynchttpclient/AHCTest.java
+++ src/test/java/jenkins/plugins/asynchttpclient/AHCTest.java
@@ -1,18 +1,46 @@
 package jenkins.plugins.asynchttpclient;
 
-import org.jvnet.hudson.test.HudsonTestCase;
+import com.ning.http.client.ListenableFuture;
+import com.ning.http.client.RequestBuilder;
+import com.ning.http.client.Response;
+import hudson.model.FreeStyleProject;
+import java.util.Random;
+import jenkins.model.Jenkins;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
-/**
- * @author stephenc
- * @since 18/12/2012 09:58
- */
-public class AHCTest  extends HudsonTestCase {
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
-    public void testSmokes() {
-        assertNotNull(AHC.instance());
+public class AHCTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void closeCausesRecycle() {
+        assertThat(AHC.instance(), notNullValue());
         AHC.instance().close();
-        assertNotNull(AHC.instance());
-        assertFalse(AHC.instance().isClosed());
+        assertThat(AHC.instance(), notNullValue());
+        assertThat(AHC.instance(), hasProperty("closed", is(false)));
+    }
+
+    @Test
+    public void worksOnJenkinsClasspath() throws Exception {
+        final Random entropy = new Random();
+        final String jobName = String.format("Random job name %d", entropy.nextLong());
+        final String systemMessage = String.format("Random system message %d", entropy.nextLong());
+        j.jenkins.setSystemMessage(systemMessage);
+        j.getInstance().createProject(j.getInstance().getDescriptorByType(FreeStyleProject.DescriptorImpl.class),
+                jobName);
+        final ListenableFuture<Response> response =
+                AHC.instance().executeRequest(new RequestBuilder("GET").setUrl(j.getURL().toURI().toString()).build());
+        assertThat(response.get().getResponseBody(), allOf(
+                containsString(Jenkins.getVersion().toString()),
+                containsString(systemMessage),
+                containsString(jobName)
+        ));
     }
 
 }

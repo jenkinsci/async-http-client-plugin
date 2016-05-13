64f64e4d37dfda7e15272f755a65881e34817b1c
Armando Fernandez <armfergom@gmail.com>
2016-05-13 13:21:41 +0200

    [JENKINS-34796] Migrate to 2.9 parent pom. Bump baseline to 1.580.1. Fix Findbugs errors.

--- src/main/java/jenkins/plugins/asynchttpclient/AHCPlugin.java
+++ src/main/java/jenkins/plugins/asynchttpclient/AHCPlugin.java
@@ -1,5 +1,6 @@
 package jenkins.plugins.asynchttpclient;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Plugin;
 import jenkins.model.Jenkins;
 
@@ -13,6 +14,7 @@ public class AHCPlugin extends Plugin {
      * {@inheritDoc}
      */
     @SuppressWarnings("unchecked")
+    @SuppressFBWarnings(value = "NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE", justification = "https://github.com/jenkinsci/jenkins/pull/2094")
     @Override
     public void stop() throws Exception {
         AHC ahc = AHC.class.cast(Jenkins.getInstance().getDescriptor(AHC.class));
